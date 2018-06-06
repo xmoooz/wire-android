ca7ba6eac4d6047152d73d1c8e2d3086e7f478be
John Nguyen <johnxuannguyen@gmail.com>
2018-06-06 12:20:41 +0200

    Adjust heading size multipliers

--- app/src/main/java/com/waz/zclient/markdown/StyleSheet.kt
+++ app/src/main/java/com/waz/zclient/markdown/StyleSheet.kt
@@ -92,7 +92,7 @@ public class StyleSheet {
      * The relative font size multiplers (values) for the various header levels (keys).
      * The header values range from 1 to 6.
      */
-    public var headingSizeMultipliers = mapOf(1 to 1.7f, 2 to 1.5f, 3 to 1.25f)
+    public var headingSizeMultipliers = mapOf(1 to 1.7f, 2 to 1.5f, 3 to 1.25f, 4 to 1.25f, 5 to 1.25f, 6 to 1.25f)
 
     /**
      * The color of a quote (including stripe).
