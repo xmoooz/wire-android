ea2e977e6818423bcdc00b11c556f99707843f7b
dean <deanrobertcook@gmail.com>
2018-07-04 14:47:01 +0200

    Avoid resetting epheemral timer on intent handling

--- app/src/main/scala/com/waz/zclient/MainActivity.scala
+++ app/src/main/scala/com/waz/zclient/MainActivity.scala
@@ -364,11 +364,11 @@ class MainActivity extends BaseActivity
   def handleIntent(intent: Intent) = {
     verbose(s"handleIntent: ${intent.log}")
 
-    def switchConversation(convId: ConvId, call: Boolean = false, exp: Option[FiniteDuration] = None) =
+    def switchConversation(convId: ConvId, call: Boolean = false, exp: Option[Option[FiniteDuration]] = None) =
       CancellableFuture.delay(750.millis).map { _ =>
         verbose(s"setting conversation: $convId")
         conversationController.selectConv(convId, ConversationChangeRequester.INTENT).map { _ =>
-          conversationController.setEphemeralExpiration(exp)
+          exp.foreach(conversationController.setEphemeralExpiration)
           if (call)
             for {
               Some(acc) <- account.map(_.map(_.userId)).head
@@ -411,7 +411,7 @@ class MainActivity extends BaseActivity
           convs <- sharingController.targetConvs.head
           exp   <- sharingController.ephemeralExpiration.head
           _     <- sharingController.sendContent(this)
-          _     <- if (convs.size == 1) switchConversation(convs.head, exp = exp) else Future.successful({})
+          _     <- if (convs.size == 1) switchConversation(convs.head, exp = Some(exp)) else Future.successful({})
         } yield clearIntent()
 
       case OpenPageIntent(page) => page match {
