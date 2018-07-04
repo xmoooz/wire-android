4c34817e715a2b80de0f22b988dd36720d1d1a23
Calum McCall <calum.mccall@wire.com>
2018-07-04 12:06:24 +0200

    Revert "Don't keep track of current notifications but request them from notification manager"
    This reverts commit 7908fdb9824176cd5fbbd1f7da504212d2d37dc8.

--- app/src/main/scala/com/waz/zclient/notifications/controllers/CallingNotificationsController.scala
+++ app/src/main/scala/com/waz/zclient/notifications/controllers/CallingNotificationsController.scala
@@ -101,11 +101,13 @@ class CallingNotificationsController(implicit cxt: WireContext, eventContext: Ev
       case (cn1, cn2) => cn1.convId.str > cn2.convId.str
     }
 
+  private var currentNotifications = Set.empty[Int]
+
   notifications.map(_.exists(!_.isMainCall)).onUi(soundController.playRingFromThemInCall)
 
   notifications.onUi { nots =>
     verbose(s"${nots.size} call notifications")
-    val toCancel = notificationManager.getActiveNotifications.map(_.getId).toSet -- nots.map(_.id).toSet
+    val toCancel = currentNotifications -- nots.map(_.id).toSet
     toCancel.foreach(notificationManager.cancel(CallNotificationTag, _))
 
     nots.foreach { not =>
@@ -148,7 +150,10 @@ class CallingNotificationsController(implicit cxt: WireContext, eventContext: Ev
         case _ => //no available action
       }
 
-      def showNotification() = notificationManager.notify(CallNotificationTag, not.id, builder.build())
+      def showNotification() = {
+        currentNotifications += not.id
+        notificationManager.notify(CallNotificationTag, not.id, builder.build())
+      }
 
       LoggedTry(showNotification()).recover {
         case NonFatal(e) =>
