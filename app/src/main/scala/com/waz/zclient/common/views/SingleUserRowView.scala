1af92fd5618eebf8a7f9dcdf2cc7098fa4f764bd
Maciej Gorywoda <makingthematrix@gmail.com>
2018-08-13 17:26:10 +0200

    Remove guest label from bots on group info page

--- app/src/main/scala/com/waz/zclient/common/views/SingleUserRowView.scala
+++ app/src/main/scala/com/waz/zclient/common/views/SingleUserRowView.scala
@@ -103,7 +103,7 @@ class SingleUserRowView(context: Context, attrs: AttributeSet, style: Int) exten
     val handle = userData.handle.map(h => StringUtils.formatHandle(h.string))
     val expiration = userData.expiresAt.map(ea => GuestUtils.timeRemainingString(ea.instant, Instant.now))
     if (showSubtitle) setSubtitle(expiration.orElse(handle)) else subtitleView.setVisibility(View.GONE)
-    setIsGuest(userData.isGuest(teamId))
+    setIsGuest(userData.isGuest(teamId) && !userData.isWireBot)
   }
 
   def setIntegration(integration: IntegrationData): Unit = {
