ec537a14c17209cd1394c8de25fbaefcad6499ae
Maciej Gorywoda <makingthematrix@gmail.com>
2018-11-15 18:29:12 +0100

    feat: Highlighting replies other than text

--- app/src/main/scala/com/waz/zclient/messages/parts/ImagePartView.scala
+++ app/src/main/scala/com/waz/zclient/messages/parts/ImagePartView.scala
@@ -27,22 +27,18 @@ import com.waz.service.downloads.AssetLoader.DownloadOnWifiOnlyException
 import com.waz.service.messages.MessageAndLikes
 import com.waz.threading.Threading
 import com.waz.zclient.common.controllers.AssetsController
-import com.waz.zclient.controllers.drawing.IDrawingController.DrawingMethod
-import com.waz.zclient.conversation.ConversationController
 import com.waz.zclient.messages.MessageView.MsgBindOptions
 import com.waz.zclient.messages.parts.assets.ImageLayoutAssetPart
-import com.waz.zclient.messages.{MessageViewPart, MsgPart}
+import com.waz.zclient.messages.{HighlightViewPart, MessageViewPart, MsgPart}
 import com.waz.zclient.utils.RichView
 import com.waz.zclient.common.views.ImageAssetDrawable.State.Failed
 import com.waz.zclient.{R, ViewHelper}
 
-class ImagePartView(context: Context, attrs: AttributeSet, style: Int) extends FrameLayout(context, attrs, style) with ImageLayoutAssetPart {
+class ImagePartView(context: Context, attrs: AttributeSet, style: Int) extends FrameLayout(context, attrs, style) with ImageLayoutAssetPart with HighlightViewPart {
   def this(context: Context, attrs: AttributeSet) = this(context, attrs, 0)
   def this(context: Context) = this(context, null, 0)
 
   override val tpe: MsgPart = MsgPart.Image
-
-  private val selection = inject[ConversationController].messages
 
   private lazy val assets = inject[AssetsController]
 
@@ -57,9 +53,6 @@ class ImagePartView(context: Context, attrs: AttributeSet, style: Int) extends F
     noW  <- noWifi
     hide <- hideContent
   } yield !hide && noW).on(Threading.Ui)(imageIcon.setVisible)
-
-  private def openDrawingFragment(drawingMethod: DrawingMethod) =
-    message.currentValue foreach (assets.openDrawingFragment(_, drawingMethod))
 
   onClicked { _ => message.head.map(assets.showSingleImage(_, this))(Threading.Ui) }
 
